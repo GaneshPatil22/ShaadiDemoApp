0dedb4068b3df48feb9e215435c3dfef0a268e0e
Ganesh patil <ganesh.patil@tudip.nl>
2020-09-28 17:27:07 +0530

    Details: - Basic App setup and initial commit.

---
+++ ShaadiDemoApp/ShaadiDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShaadiDemoApp
+//
+//  Created by MacMini 20 on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
